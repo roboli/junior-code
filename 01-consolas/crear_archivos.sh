30eecdaa7f7eceb70410f3f5bdce3768761ceede
roboli <robertooliveros@mac.com>
2022-08-18 16:13:40 -0600

    Cambiar value por extension

--- 01-consolas/crear_archivos.sh
+++ 01-consolas/crear_archivos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for value in txt js php
+for extension in txt js php
 do
-  echo "prueba" > ./mis_archivos/prueba_1.$value
-  echo "prueba" > ./mis_archivos/prueba_2.$value
+  echo "prueba" > ./mis_archivos/prueba_1.$extension
+  echo "prueba" > ./mis_archivos/prueba_2.$extension
 done
