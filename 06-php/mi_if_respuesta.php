f75c87c295876b5f357c0e81c24eaa0cb3267d68
roboli <robertooliveros@mac.com>
2022-09-27 16:50:47 -0600

    Agregar ejemplo if formulario

---
+++ 06-php/mi_if_respuesta.php
@@ -0,0 +1,29 @@
+<?php
+  $numero = $_POST['valor_x'];
+  $respuesta = '';
+  $es_correcta = false;
+
+  if ($numero == '2') {
+    $respuesta = 'Acetaste! Eres un genio.';
+    $es_correcta = true;
+  } elseif ($numero == '1' || $numero == '3') {
+    $respuesta = 'Casi, intenta de nuevo.';
+  } else {
+    $respuesta = 'Oye, tienes que ponerte a estudiar!';
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Respuesta Algebra</title>
+  </head>
+  <body>
+    <?php echo($respuesta); ?>
+    <?php if (!$es_correcta): ?>
+    <a href="http://localhost/ejercicios/mi_if_formulario.html" />
+       Regresar
+    </a>
+    <?php endif; ?>
+  </body>
+</html>
