b035ba945bdbf2406e6d57e7f3fae36eebf5d358
roboli <robertooliveros@mac.com>
2023-01-31 05:15:37 -0600

    Agregar nuevo ejemplo

---
+++ 10-php&js/10.2.2/resultados.php
@@ -0,0 +1,14 @@
+<?php
+  $saga = $_POST['saga'];
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Resultados</title>
+  </head>
+  <body>
+    <h3>Gracias por tu voto.</h3>
+    <p>Votaste por: <b><?php echo($saga) ?></b></p>
+  </body>
+</html>
