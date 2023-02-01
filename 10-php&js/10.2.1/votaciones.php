cad2d7fe623c3279449ac142a1db398557bc874e
roboli <robertooliveros@mac.com>
2023-02-01 06:01:40 -0600

    Renombrar saga por voto

--- 10-php&js/10.2.1/votaciones.php
+++ 10-php&js/10.2.1/votaciones.php
@@ -1,5 +1,5 @@
 <?php
-  $saga = $_POST['saga'];
+  $voto = $_POST['voto'];
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +7,7 @@
   <head>
     <title>Votaciones</title>
   </head>
-  <?php if (empty($saga)): ?>
+  <?php if (empty($voto)): ?>
   <script>
     function escogerSaga() {
       let boton = document.getElementById('votar');
@@ -23,17 +23,17 @@
     function aplicarLeyoTodas() {
       let boton = document.getElementById('votar');
       let checkbox = document.getElementById('leyo_todas');
-      let radios = document.getElementsByName('saga');
-      let haySaga;
+      let radios = document.getElementsByName('voto');
+      let hayVoto;
 
       for (var i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
-          haySaga = true;
+          hayVoto = true;
           break;
         }
       }
 
-      if (checkbox.checked && haySaga) {
+      if (checkbox.checked && hayVoto) {
         boton.removeAttribute('disabled');
       } else {
         boton.setAttribute('disabled', true);
@@ -42,14 +42,14 @@
   </script>
   <?php endif; ?>
   <body>
-    <?php if (empty($saga)): ?>
+    <?php if (empty($voto)): ?>
     <form action="votaciones.php" method="post">
       <p>Ingresa tu voto:</p>
-      <input id="hp" type="radio" name="saga" value="Harry Potter" onclick="escogerSaga();">
+      <input id="hp" type="radio" name="voto" value="Harry Potter" onclick="escogerSaga();">
       <label for="hp">Harry Potter</label><br />
-      <input id="pj" type="radio" name="saga" value="Percy Jackson" onclick="escogerSaga();">
+      <input id="pj" type="radio" name="voto" value="Percy Jackson" onclick="escogerSaga();">
       <label for="pj">Percy Jackson</label><br />
-      <input id="ew" type="radio" name="saga" value="Ender Wigin" onclick="escogerSaga();">
+      <input id="ew" type="radio" name="voto" value="Ender Wigin" onclick="escogerSaga();">
       <label for="ew">Ender Wigin</label><br /><br />
       <input type="checkbox" id="leyo_todas" name="leyo_todas" value="leyo_todas" onchange="aplicarLeyoTodas();">
       <label for="leyo_todas"> Confirmo que lei todas las sagas</label><br /><br />
@@ -57,7 +57,7 @@
     </form>
     <?php else: ?>
     <h3>Gracias por tu voto.</h3>
-    <p>Votaste por: <b><?php echo($saga) ?></b></p>
+    <p>Votaste por: <b><?php echo($voto) ?></b></p>
     <?php endif; ?>
   </body>
 </html>
